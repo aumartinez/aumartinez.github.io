4b676ef5d60b5280cbd2126a1baf1fe3c72603b6
Augusto Martinez <acm_asd@hotmail.com>
2020-04-02 15:34:48 -0600

    updates

---
+++ js/contact.js
@@ -0,0 +1,22 @@
+//Form validation
+
+$(document).ready(function(){
+  //To do
+  var url = $("#contact-form").attr("action");
+  
+  $("#contact-form").submit(function(evt){
+    $(".loader").addClass("active");
+    
+    var errors = validateForm();
+    
+    if (errros.length == 0) {
+      $.post(url);
+    }
+    else {
+      removeErrors();
+      displayErrors(errors);
+      evt.preventDefault();
+      $(".loader").removeClass("active");
+    }
+  });
+});
