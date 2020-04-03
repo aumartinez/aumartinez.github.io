3343c6a2546636ace76ec56d5072590e08d796a4
Augusto Martinez <acm_asd@hotmail.com>
2020-04-02 18:34:58 -0600

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -25,6 +25,30 @@ $(document).ready(function(){
     var arr = [];
     
     //Required
+    if($("#name").val().length == 0) {
+      arr.push("name");
+    }
+    if ($("#email").val().length == 0) {
+      arr.push("email");
+    }
+    if ($("#message").val().length == 0) {
+      arr.push("message");
+    }
     
+    return arr;
   }
+  
+  function displayErrors(errors) {
+    for (var i = 0; i < errors.length; i++) {
+      $("#" + errors[i]).next().addClass("active");
+    }
+    
+    $("#error-div").addClass("active");
+    $("#error-div").text("Errors found!");
+  }
+  
+  function removeErrors() {
+    $(".error-mess.active").removeClass("active");
+  }
+  
 });
